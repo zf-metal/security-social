8f44f6ed677c708111ec7ddad6c6a8b65155e120
Alejandro Furgeri <alejandro.furgeri@sondeos.com.ar>
2017-03-13 21:39:59 -0300

    Update

--- src/Module.php
+++ src/Module.php
@@ -10,4 +10,7 @@ class Module {
     public function getConfig() {
         return include __DIR__ . '/../config/module.config.php';
     }
+
+    public function onBootstrap(EventInterface $e) {
+    }
 }
